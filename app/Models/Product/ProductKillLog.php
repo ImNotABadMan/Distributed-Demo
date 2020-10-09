67313a8457ca5d30d86057fb2627931d7e339333
binz <binz123456>
2020-10-09 23:03:04 +0800

    log

---
+++ app/Models/Product/ProductKillLog.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Product;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductKillLog extends Model
+{
+    protected $table = "ss_products_kill_log";
+
+    protected $primaryKey = "id";
+}
