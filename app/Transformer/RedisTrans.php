f9787356a7e778ae62a4f377eaea0433eeca7832
binz <binz123456>
2020-10-13 20:24:47 +0800

    substr

--- app/Transformer/RedisTrans.php
+++ app/Transformer/RedisTrans.php
@@ -6,6 +6,8 @@ namespace App\Transformer;
 
 class RedisTrans
 {
+    private $maxStrLen = 255;
+
     public function transKeyValue(array $data)
     {
         $arr = [];
@@ -14,7 +16,8 @@ class RedisTrans
             if (is_array($item[1])) {
                 $value = implode("|><|", $item[1]);
             } else {
-                $value = $item[1];
+                $value = is_string($item[1]) && strlen($item[1]) > $this->maxStrLen ?
+                    substr($item[1], 0, $this->maxStrLen) : $item[1];
             }
 
             $arr[$index] = [
