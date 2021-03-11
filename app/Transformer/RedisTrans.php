0308da71e5ea71e3bd94aaf998825ef173bce7a3
binz <bin_zheng@limei-trading.com>
2021-03-11 13:33:12 +0800

    编码转换

--- app/Transformer/RedisTrans.php
+++ app/Transformer/RedisTrans.php
@@ -17,10 +17,14 @@ class RedisTrans
             if (isset($item[1])) {
 
                 if (is_array($item[1])) {
-                    $value = implode("|><|", $item[1]);
+                    $encodingStrArr = array_map(function ($value) {
+                        return mb_convert_encoding($value, 'utf8', mb_detect_encoding($value));
+                    }, $item[1]);
+                    $value = implode("|><|", $encodingStrArr);
                 } else {
                     $value = is_string($item[1]) && strlen($item[1]) > $this->maxStrLen ?
                         substr($item[1], 0, $this->maxStrLen) : $item[1];
+                    $value = mb_convert_encoding($value, 'utf8', mb_detect_encoding($value));
                 }
             }
 
